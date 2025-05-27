5927f07c2e37d53c236a95a4a7aba86fa9e7475b
RachitSrivastava12 <rachitsrivastava76@gmail.com>
2025-05-27 19:19:37 +0530

    Removed Vercel Analytics

--- Indexer/project/src/main.tsx
+++ Indexer/project/src/main.tsx
@@ -2,9 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { inject } from '@vercel/analytics';
 
-inject();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
